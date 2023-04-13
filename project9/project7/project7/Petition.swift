56f98095da8525891c3c8c4923fe5d44f01097cf
Artem <artchzhen@gmail.com>
2023-04-13 19:17:28 +0900

    initial commit devolopment from project7

---
+++ project9/project7/project7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  project7
+//
+//  Created by Артем Чжен on 11/04/23.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ project9/project7/project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  project7
+//
+//  Created by Артем Чжен on 11/04/23.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
